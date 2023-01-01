265ac7fc78eeada034a532b7174752ac7659e9f2
MBayu2107 <mbayu4116@gmail.com>
2023-01-01 18:12:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// Variabel untuk menyimpan nilai yang ditampilkan di layar
+var layar = '';
+
+// Fungsi untuk menambahkan angka atau simbol ke layar
+function masukkan(input) {
+    layar += input;
+    document.getElementById('layar').innerHTML = layar;
+}
+
+// Fungsi untuk menghitung hasil dari operasi matematika
+function hitung() {
+    layar = eval(layar);
+    document.getElementById('layar').innerHTML = layar;
+}
+
+// Fungsi untuk membersihkan layar
+function bersihkan() {
+    layar = '';
+    document.getElementById('layar').innerHTML = layar;
+}
+
+// Fungsi untuk menghitung sinus sudut (dalam radian)
+function sinus() {
+    var sudut = parseFloat(layar);
+    var hasil = Math.sin(sudut);
+    document.getElementById('layar').innerHTML = hasil;
+}
+
+// Fungsi untuk menghitung cosinus
+function cosinus() {
+    var sudut = parseFloat(layar);
+    var hasil = Math.cos(sudut);
+    document.getElementById('layar').innerHTML = hasil;
+}
+
+// Fungsi untuk menghitung tangen
+function tangen() {
+    var sudut = parseFloat(layar);
+    var hasil = Math.tan(sudut);
+    document.getElementById('layar').innerHTML = hasil;
+}
+
+
+
